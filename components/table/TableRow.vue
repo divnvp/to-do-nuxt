c743b5ebf275fd37adf8cb1f9a39adebddfa97fd
Diana <divnvp@yandex.com>
2022-07-25 19:59:57 +0900

    add: table

---
+++ components/table/TableRow.vue
@@ -0,0 +1,110 @@
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  record: { type: Object, required: true }
+});
+</script>
+
+<template>
+  <div class="tr-row">
+    <div class="tr-col" style="width: 100px;">
+      <input type="checkbox" class="checkbox-circle">
+    </div>
+
+    <div class="tr-col">
+      <span class="tr-col__record">
+        {{ record.name }}
+      </span>
+    </div>
+    
+    <div class="tr-col">
+      <span
+        class="tr-col__status"
+        :style="{ color: record.status == 'Выполнено' ? '#134EC1' : '#F89B11' }"
+      >
+        {{ record.status }}
+      </span>
+    </div>
+
+    <div class="tr-col">
+      <span>
+        {{ record.date }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.tr-row {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+
+  padding: 20px 40px;
+
+  border-top: 1px solid #EEEBE9;
+  border-bottom: 1px solid #EEEBE9;
+
+  font-family: "VelaSans", sans-serif;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 132%;
+
+  &:hover {
+    background: #F6F9FF;
+  }
+
+  &:hover input[type="checkbox"] {
+    border: 1px solid #134EC1;
+    box-shadow: 0px 4px 4px rgba(19, 78, 193, 0.15);
+  }
+}
+
+.tr-col {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+
+  padding-right: 41px;
+
+  &__record {
+    width: calc(937px - 15px * 3 - 20px * 2);
+
+    margin-left: 15px;
+  }
+
+  &__status {
+    width: calc(151px - 15px);
+  }
+}
+
+.checkbox-circle {
+  width: 20px;
+  height: 20px;
+
+  border-radius: 50%;
+
+  border: 1px solid #16191D;
+
+  vertical-align: middle;
+  appearance: none;
+  -webkit-appearance: none;
+  outline: none;
+
+  cursor: pointer;
+
+  &:checked {
+    border: 1px solid #134EC1;
+    box-shadow: 0px 4px 4px rgba(19, 78, 193, 0.15);
+  }
+
+  &:checked:after {
+    content: url("../../static/ok.png");
+    margin-left: 3px;
+    outline: none;
+    border: none;
+  }
+} 
+</style>
---
+++ components/table/UITable.vue
@@ -0,0 +1,122 @@
+<script setup>
+import TableRow from './TableRow.vue';
+
+const records = [
+  { id: 1, name: "Размещение новостей на сайте", status: "Выполнено", date: "22.04.2022" },
+  { id: 2, name: "Внедрить Wi-fi для читателей", status: "В работе", date: "22.04.2022" },
+  { id: 3, name: "Отредактировать раздел “Доступная среда”", status: "Выполнено", date: "22.04.2022" },
+  { id: 4, name: "Презентация “Информационные технологии”", status: "В работе", date: "22.04.2022" },
+  { id: 5, name: "Счётчики — внедрить дизайн", status: "В работе", date: "22.04.2022" },
+  { id: 6, name: "Сверстать новый layout", status: "В работе", date: "22.04.2022" },
+  { id: 7, name: "Скролл в новостях", status: "Выполнено", date: "22.04.2022" },
+  { id: 8, name: "Форма сброса пароля", status: "В работе", date: "22.04.2022" },
+  { id: 9, name: "Внедрение модуля Chat", status: "Выполнено", date: "22.04.2022" }
+]
+</script>
+
+<template>
+  <section>
+    <header class="table-header">
+      <aside class="table-header__name">
+        <hr />
+        <span>Описание</span>
+      </aside>
+      
+      <aside class="table-header__name" style="margin-left: 58%">
+        <hr />
+        <span>Статус</span>
+      </aside>
+
+      <aside class="table-header__name" style="margin-right: 6%">
+        <hr />
+        <span>Дата</span>
+      </aside>
+    </header>
+
+    <div class="table-body">
+      <TableRow
+        v-for="record in records"
+        :key="record.id"
+        :record="record"
+      />
+    </div>
+  </section>
+</template>
+
+<style lang="scss">
+header {
+  display: flex;
+}
+
+.col {
+  display: flex;
+  flex: 1;
+}
+
+.table {
+  font-family: "VelaSans", sans-serif;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 132%;
+  color: #16191D;
+
+  hr {
+    padding: 0;
+    margin-right: 20px;
+
+    width: 1px;
+    height: 32px;
+
+    border: none;
+    border-left: 1px solid #C4C4C4;
+  }
+
+  &-row {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+  }
+
+  &-header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1 1 0;
+
+    margin-bottom: 17px;
+    margin-left: 100px;
+
+    font-family: "VelaSans", sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 132%;
+
+    &__name {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: flex-start;
+    }
+
+    hr {
+      padding: 0;
+      margin-right: 20px;
+
+      width: 1px;
+      height: 32px;
+
+      border: none;
+      border-left: 1px solid #C4C4C4;
+    }
+  }
+
+  &-body {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    flex: 1 1 0;
+  }
+}
+</style>
