6c0a5abb542372e2e42ab796fb8ff572ae63357a
Diana <divnvp@yandex.com>
2022-07-25 14:21:57 +0900

    add: ToDo component

---
+++ components/ToDo.vue
@@ -0,0 +1,21 @@
+<script setup>
+import UIHeader from './UIHeader.vue';
+import UIMain from './UIMain.vue';
+
+</script>
+
+<template>
+  <div>
+    <header>
+      <UIHeader />
+    </header>
+
+    <main>
+      <UIMain />
+    </main>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
