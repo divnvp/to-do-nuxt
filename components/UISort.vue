65c25290814168594372e927c29c6bc30025ee0b
Diana <divnvp@yandex.com>
2022-07-25 14:40:20 +0900

    add: sort component

---
+++ components/UISort.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+</script>
+
+<template>
+  <article class="sort">
+    <span>Сортировать по:</span>
+
+    <select class="sort__select">
+      <optgroup>
+        <option
+          selected
+        >
+          Дата
+        </option>
+      </optgroup>
+    </select>
+  </article>
+</template>
+
+<style lang="scss">
+.sort {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  font-weight: 400;
+  font-size: 14px;
+
+  &__select {
+    font-weight: 400;
+    font-size: 14px;
+
+    margin-left: 15px;
+
+    border: none;
+  }
+}
+
+optgroup {
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 132%;
+  color: #16191D;
+}
+</style>
