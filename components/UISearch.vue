25d129678779a100ca92c8d38f708460af983819
Diana <divnvp@yandex.com>
2022-07-25 12:48:51 +0900

    add: search section

---
+++ components/UISearch.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+<template>
+  <article class="search">
+    <img src="../static/search.png">
+
+    <input
+      placeholder="Поиск ID, Имени, статуса или даты"
+      class="search__input"
+    />
+  </article>
+</template>
+
+<style lang="scss">
+.search {
+  display: flex;
+  align-items: center;
+}
+
+.search__input {
+  margin-left: 16px;
+  min-width: 227px;
+
+  border: none;
+}
+
+.search__input:focus {
+  outline: none;
+}
+</style>
