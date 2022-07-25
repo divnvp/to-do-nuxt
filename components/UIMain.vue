c21c4b190c25a3455280921e086172671fb7dd05
Diana <divnvp@yandex.com>
2022-07-25 12:48:32 +0900

    add: main section

---
+++ components/UIMain.vue
@@ -0,0 +1,23 @@
+<script setup>
+import UISearch from './UISearch.vue';
+import UISort from './UISort.vue';
+
+</script>
+
+<template>
+  <section class="list-actions">
+    <UISearch />
+
+    <UISort />
+  </section>
+</template>
+
+<style lang="scss">
+.list-actions {
+  display: flex;
+  justify-content: space-between;
+  flex: 1 1 0;
+
+  margin-top: 34px;
+}
+</style>
